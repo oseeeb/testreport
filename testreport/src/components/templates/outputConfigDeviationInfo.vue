aac0f950a0f1a905a235eb42ef2f15b237dc154e
Eboua Osée <ebouapane@gmail.com>
2021-08-08 20:44:10 +0100

    update overview

---
+++ testreport/src/components/templates/outputConfigDeviationInfo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <span>{{configCount+' configuration'+(configCount>1?'s have':' has')+' open deviations'}}</span>
+    </div>
+</template>
+<script>
+export default {
+    name:'OutputConfDevInfo',
+    props:{
+        configCount:{
+            type:Number,
+            default:0
+        }
+    }
+}
+</script>
--- testreport/src/pages/Overview.vue
+++ testreport/src/pages/Overview.vue
@@ -13,93 +13,160 @@
         </md-table>
       </div>  
       <div class="md-layout-item md-xlarge-size-25  md-large-size-25 md-medium-size-25 md-small-size-50 md-xsmall-size-100">
-        <md-card class="md-accent" md-with-hover>
+        <md-card :class="Result_OverallResult?'md-success':'md-accent'" md-with-hover>
           <md-ripple>
             <md-card-header>
               <div class="md-title">Test Result</div>
             </md-card-header>
             <md-card-content>
-              <span style="">FAIL</span>
+              <span style="">{{Result_OverallResult?'OK':'FAIL'}}</span>
             </md-card-content>
           </md-ripple>
         </md-card>
       </div>  
     </div>
     <div class="md-layout">
-      <md-card class="md-layout-item">
-        <md-card-header>
-          <div class="md-title">Test Plan</div>
-        </md-card-header>
+      <div class="md-layout-item">
+        <md-card>
+          <md-card-header>
+            <div class="md-title">Test Plan</div>
+          </md-card-header>
+          <md-card-content>
+              <span>{{plannedTestCase.length}} testCases</span><br/>
+              <span>{{plannedTestRun.length}} testRun</span><br/>
+              <span>{{unplannedTestCase.length}} Unplanned TestCase</span><br/>
+          </md-card-content>
+        </md-card>
+        <md-card>
+          <md-card-header>
+            <div class="md-title">Functional TestCases</div>
+          </md-card-header>
+          <md-card-content>
+              <span>{{plannedTestCase.length}} testCases</span><br/>
+              <span>{{plannedTestRun.length}} testRun</span><br/>
+          </md-card-content>
+        </md-card>
+      </div>
+      <div class="md-layout-item">
+        <md-card>
+          <md-card-header>
+            <div class="md-title">Test Cases</div>
+          </md-card-header>
 
-        <md-card-content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-        </md-card-content>
-      </md-card>
-      <md-card class="md-layout-item">
-        <md-card-header>
-          <div class="md-title">Test Cases</div>
-        </md-card-header>
-
-        <md-card-content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-        </md-card-content>
-      </md-card>
-      <md-card class="md-layout-item">
-        <md-card-header>
-          <div class="md-title">Test Configuration</div>
-        </md-card-header>
-
-        <md-card-content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-        </md-card-content>
-      </md-card>
-      <md-card class="md-layout-item">
-        <md-card-header>
-          <div class="md-title">Test Level</div>
-        </md-card-header>
-
-        <md-card-content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-        </md-card-content>
-      </md-card>  
-    </div>
-    <div class="md-layout">
-      <md-card class="md-layout-item">
-        <md-card-header>
-          <div class="md-title">Functional TestCases</div>
-        </md-card-header>
-
-        <md-card-content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-        </md-card-content>
-      </md-card>
-      <div class="md-layout md-layout-item">
+          <md-card-content>
+              <span>{{$store.state.testCases.length}} testCases</span><br/>
+              <span>{{$store.state.testRuns.length}} testRun</span><br/></md-card-content>
+        </md-card>
         <md-card >
           <md-card-header>
             <div class="md-title">RunTime Coverage</div>
           </md-card-header>
-
           <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+            <span class="symbol_ok"></span> <span>Runtime Coverage (Component Test)</span><br/>
+            <span class="symbol_ok"></span> <span>Runtime Coverage (Unit Test)</span><br/>
+            <md-table>
+              <md-table-row >
+                <md-table-cell style="width:33%;">FC</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row><md-table-row >
+                <md-table-cell style="width:33%;">SC</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:33%;">BC</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:33%;">DC</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:33%;">CC</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:33%;">Overall_FC</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:33%;">Overall_SC</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:33%;">Overall_BC</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:33%;">Overall_DC</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:33%;">Overall_CC</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:33%;">FC_Component</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:33%;">SC_Component</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:33%;">CC_Component</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:33%;">DC_Component</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:33%;">BC_Component</md-table-cell>
+                <md-table-cell style="width:33%;">PTH</md-table-cell>
+                <md-table-cell style="width:34%;">PTH</md-table-cell>
+              </md-table-row>
+            </md-table>
           </md-card-content>
         </md-card>
         <md-card >
           <md-card-header>
             <div class="md-title">Preprocessor Coverage</div>
           </md-card-header>
-
           <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+            <span>{{TestRuns_PPCoverage.length}} TestRuns</span>
           </md-card-content>
         </md-card>
       </div>
-      <div class="md-layout md-layout-item">
+      <div class="md-layout-item">
+        <md-card>
+          <md-card-header>
+            <div class="md-title">Test Configuration</div>
+          </md-card-header>
+          <md-card-content>
+            <span>{{testConfigs.length}} TestConfigs</span> 
+          </md-card-content>
+        </md-card>
         <md-card >
           <md-card-header>
             <div class="md-title">MISRA</div>
           </md-card-header>
           <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+            <OutputConfDevInfo :configCount="configurationCount"/>
+            <span>{{TestRuns_QACSummary.length}} Tests Run</span>
           </md-card-content>
         </md-card>
         <md-card >
@@ -108,7 +175,7 @@
           </md-card-header>
 
           <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+            <span>{{TestRuns_MemoryResources.length}} Tests run</span>
           </md-card-content>
         </md-card>
         <md-card >
@@ -117,7 +184,7 @@
           </md-card-header>
 
           <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+           <span>{{TestRuns_PCLint.length}} TestRuns</span>
           </md-card-content>
         </md-card>
         <md-card >
@@ -126,7 +193,7 @@
           </md-card-header>
 
           <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+            <span>{{TestRuns_BuildEmbeddedCompiler.length}} TestRuns</span>
           </md-card-content>
         </md-card>
         <md-card >
@@ -135,31 +202,109 @@
           </md-card-header>
 
           <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+            <span>{{TestRuns_BuildVisual.length}} TestRuns</span>
           </md-card-content>
         </md-card>
       </div>
-      <div class="md-layout md-layout-item">
+      <div class="md-layout-item">
+        <md-card>
+          <md-card-header>
+            <div class="md-title">Test Level</div>
+          </md-card-header>
+          <md-card-content>
+          </md-card-content>
+        </md-card> 
         <md-card>
           <md-card-header>
             <div class="md-title">CodeMetric</div>
           </md-card-header>
-
           <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+            <span>{{TestRuns_QACSummary.length}} Tests Run</span>
+           <md-table>
+              <md-table-row >
+                <md-table-cell style="width:50%;">PTH</md-table-cell>
+                <md-table-cell style="width:50%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:50%;">CYC</md-table-cell>
+                <md-table-cell style="width:50%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:50%;">CAL</md-table-cell>
+                <md-table-cell style="width:50%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:50%;">PAR</md-table-cell>
+                <md-table-cell style="width:50%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:50%;">MIF</md-table-cell>
+                <md-table-cell style="width:50%;">PTH</md-table-cell>
+              </md-table-row>
+              <md-table-row >
+                <md-table-cell style="width:50%;">LOC</md-table-cell>
+                <md-table-cell style="width:50%;">PTH</md-table-cell>
+              </md-table-row>
+            </md-table>
           </md-card-content>
         </md-card>
-      </div>
-      
+
+      </div> 
     </div>
   </div>
 </template>
 <script>
+import OutputConfDevInfo from '../components/templates/outputConfigDeviationInfo.vue'
 export default {
+  components:{
+    OutputConfDevInfo
+  },
   data() {
     return {
-      infoReport:[]
+      infoReport:[],
+      plannedTestCase:[],
+      unplannedTestCase:[],
+      plannedTestRun:[],
+      unplannedTestRun:[],
+      testConfigs:[],
+      TestRuns_QACSummary:[],
+      TestRuns_MemoryResources:[],
+      configurationCount:0,
+      TestRuns_PCLint:[],
+      TestRuns_BuildEmbeddedCompiler:[],
+      TestRuns_BuildVisual:[],
+      TestRuns_PPCoverage:[],
+      TestCases_WithResult:[],
+      TestCases_ThisCycle:[],
+      TestCases_ThisCycle_Passed:[],
+      TestCases_ThisCycle_NotPassed_Justified:[],
+      NrOf_TestCases_Planned_ThisCycle:0,
+      NrOf_TestCases_Planned_ThisCycle_Passed:0,
+      NrOf_TestCases_Planned_ThisCycle_NotPassed_Justified:0
     };
+  },
+  computed:{
+    Result_OverallResult(){
+      var Result_TestPlanStatus = this.plannedTestRun.lenght !== 0
+      var Result_TestPlanCoverage = this.NrOf_TestCases_Planned_ThisCycle===this.plannedTestCase.length
+      var Result_TestPlanTestResult = this.NrOf_TestCases_Planned_ThisCycle===(this.NrOf_TestCases_Planned_ThisCycle_Passed+this.NrOf_TestCases_Planned_ThisCycle_NotPassed_Justified)
+      
+      console.log('NrOf_TestCases_Planned_ThisCycle',this.NrOf_TestCases_Planned_ThisCycle)
+      console.log('plannedTestCase',this.plannedTestCase.length)
+      return Result_TestPlanStatus&&Result_TestPlanCoverage&&Result_TestPlanTestResult;
+    }
+  },
+  methods:{
+    findTestRun(elt,array){
+      elt.forEach(element => {
+        if(element['testrun']){
+          var testrun = Object.values(element.testrun);
+          array.push(...testrun)
+        }else if(Array.isArray(element)){
+          array.push(...element)
+        }
+      });  
+    }
   },
   mounted(){
   var testrunwithInfo = []
@@ -195,6 +340,145 @@ export default {
       value:''
     },
   ]
+
+  this.$store.state.testCases.forEach(element=>{
+    if(element._attributes&&element._attributes.ExecPlan==='x'){
+      this.plannedTestCase.push(element)
+    }else{
+      this.unplannedTestCase.push(element)
+    }
+  })
+
+  this.findTestRun(this.plannedTestCase,this.plannedTestRun)
+  this.findTestRun(this.unplannedTestCase,this.unplannedTestRun)
+
+  this.$store.state.testRuns.forEach(element=>{
+    if(element._attributes&&element._attributes.config){
+      this.testConfigs.push(element._attributes.config)
+    }
+  })
+
+  this.testConfigs = [...(new Set(this.testConfigs))]
+  
+  this.$store.state.testRuns.forEach(element => {
+    if(element['log_QACSummary']){
+      this.TestRuns_QACSummary.push(element);
+    }
+  })
+  
+  this.$store.state.testRuns.forEach(element => {
+    if(element['log_MemoryResources']){
+      this.TestRuns_MemoryResources.push(element);
+    }
+  })
+
+  this.$store.state.testRuns.forEach(element => {
+    if(element['varCoverage']){
+      this.TestRuns_PPCoverage.push(element);
+    }
+  })
+
+  this.$store.state.testCases.forEach(elemt=>{
+    var testruns = []
+    if(elemt['testrun']){
+      testruns.push(...Object.values(elemt.testrun))
+      testruns=testruns.filter(elt=>{
+        return elt.result!==undefined
+      })
+      if(testruns.lenght!==0){
+        this.TestCases_WithResult.push(elemt)
+      }
+    }
+  })
+
+  
+  this.TestCases_WithResult.forEach(elemt=>{
+    var testruns = []
+    if(Array.isArray(elemt['testrun'])){
+      testruns.push(...elemt.testrun)
+      testruns=testruns.filter(elt=>{
+        return elt._attributes.execCnt!==0
+      })
+      if(testruns.lenght!==0){
+        this.TestCases_ThisCycle.push(elemt)
+      }
+    }
+  })
+
+   this.TestCases_ThisCycle.forEach(elemt=>{
+    var testruns = []
+    if(elemt['testrun']){
+      testruns.push(...Object.values(elemt.testrun))
+      testruns=testruns.filter(elt=>{
+        return elt.result._text!=='ok'&&elt.result._text!=='N/A'
+      })
+      if(testruns.lenght===0){
+        this.TestCases_ThisCycle_Passed.push(elemt)
+      }
+    }
+  })
+
+  this.TestCases_ThisCycle.forEach(elemnt=>{
+    var testruns = []
+    if(elemnt['testrun']){
+      testruns.push(...Object.values(elemnt.testrun))
+      testruns=testruns.filter(elt=>{
+        return (elt.result._text!=='ok'&&elt.result._text!=='N/A')&&(elt.justification&&elt.justification.length!==0)
+      })
+      if(testruns.lenght!==0){
+        this.TestCases_ThisCycle_NotPassed_Justified.push(elemnt)
+      }
+    }
+  })
+
+  this.NrOf_TestCases_Planned_ThisCycle =  this.TestCases_ThisCycle.filter(elemt=>{
+    return elemt._attributes.ExecPlan==='x'
+  }).length
+
+  console.log('test with result',this.TestCases_WithResult)
+
+  this.NrOf_TestCases_Planned_ThisCycle_Passed =  this.TestCases_ThisCycle_Passed.filter(elemt=>{
+    return elemt._attributes.ExecPlan==='x'
+  }).length
+
+  this.NrOf_TestCases_Planned_ThisCycle_NotPassed_Justified = this.TestCases_ThisCycle_NotPassed_Justified.filter(elemt=>{
+    return elemt._attributes.ExecPlan==='x'
+  }).length
+
+  // console.log('This cycle tes',this.TestRuns_QACSummary)
+  var test = []
+  this.TestRuns_QACSummary.forEach(elt=>{
+    if(elt.log_build.message&&Array.isArray(elt.log_build.message)){
+      var msg = elt.log_build.message.filter(elt=>{
+        return elt.result._text!=='info'
+      })
+      if(msg.length!==0){
+        test.push(elt)
+      }
+    }
+  })
+
+  this.configurationCount = test.length
+
+  this.$store.state.testRuns.forEach(element => {
+    if(element['log_build']&&element['log_build']._type==='CheckPClint'){
+      this.TestRuns_PCLint.push(element);
+    }
+  })
+  
+  this.$store.state.testRuns.forEach(element => {
+    if(element['log_build']&&element['log_build']._type==='EmbeddedCompiler'&&element['testcase_attr'].name==='Build VC8'&&element['testcase_attr'].name==='Build VC'){
+      this.TestRuns_BuildEmbeddedCompiler.push(element);
+    }
+  })
+
+  this.$store.state.testRuns.forEach(element => {
+    if(element['log_build']&&element['log_build']._type==='Visual'&&element['testcase_attr'].name==='Build VC8'&&element['testcase_attr'].name==='Build VC'){
+      this.TestRuns_BuildVisual.push(element);
+    }
+  })
+
+  console.log('testRuns_QACSummary', this.TestRuns_QACSummary)
   }
 };
 </script>
@@ -207,6 +491,9 @@ export default {
     line-height: 5px !important;
   }
   .md-card-header{
-    padding: 0 !important;
+    padding: 5px !important;
+  }
+  .md-title{
+    font-size: 0.9em !important;
   }
 </style>
