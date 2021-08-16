9088ed4377364f78348909988d8165548ab49439
Eboua Osée <ebouapane@gmail.com>
2021-08-16 06:14:47 +0100

    update mat

--- testreport/src/components/RecursiveAccordion.vue
+++ testreport/src/components/RecursiveAccordion.vue
@@ -13,57 +13,27 @@
                     <div v-if="testgroup.testgroup">
                         <recursive-accordion :testgroupInit="Object.values(testgroup.testgroup)" :sharename="(testgroup._attributes?testgroup._attributes.name.replace(/ /g,'-'):'no-name')" :level="level+1"></recursive-accordion>
                     </div>
-                    <div v-if="testgroup.testcase" style="width:100%">
-                        <table class="fold-table">
-                            <thead>
-                                <tr>
-                                <th>TestCase ID</th>
-                                <th>Name</th>
-                                <th>Result</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <div style="width:100%;" v-for="(item,key) in testgroup.testcase" :key="key" >
-                                <tr class="view">
-                                    <td style="width:25%;"><span :data-tooltip="item.description?item.description._cdata:''">{{ item._attributes?item._attributes.id:'no-id' }}</span></td>
-                                    <td style="width:50%;">{{ item._attributes?item._attributes.name:'no-name'}}</td>
-                                    <!-- <md-table-cell :style="'text-align:center;width:25%;background-color:'+(Array.isArray(item.testrun)?(item.testrun.filter(elt=>{
-                                        if(Array.isArray(elt.result)){
-                                          return  elt.result[0]._text.includes('ok')
-                                        }else{
-                                           return elt.result._text.includes('ok')
-                                        }
-                                    }).length===item.testrun.length?'#00FF00;':'red;'):(Array.isArray(item.testrun.result)?(item.testrun.result[0]._text.includes('ok')?'#00FF00;':'red;'):item.testrun.result._text.includes('ok')?'#00FF00;':'red;'))">{{ Array.isArray(item.testrun)?(item.testrun.filter(elt=>{
-                                        if(Array.isArray(elt.result)){
-                                          return  elt.result[0]._text.includes('ok')
-                                        }else{
-                                           return elt.result._text.includes('ok')
-                                        }
-                                    }).length===item.testrun.length?'OK':'FAIL'):item.testrun.result._text}}</md-table-cell> -->
-                                    <td></td>
-                                </tr>
-                                <tr class="fold">
-                                    <table>
-                                        <tr v-for="(testrun,kex) in item.testrun" :key="kex">
-                                            <td style="width:25%;"><span :data-tooltip="testrun._attributes?testrun._attributes.config:'no-id'">{{ testrun._attributes?testrun._attributes.config:'no-id' }}</span></td>
-                                            <td style="width:50%;">-</td>
-                                            <td style="width:50%;">-</td>
-                                            <!-- <md-table-cell :style="'text-align:center;width:25%;background-color:'+(testrun.result&&testrun.result._text.includes('ok')?'#00FF00;':'red;')">{{ testrun.result?testrun.result._text:'' }}</md-table-cell> -->
-
-                                        </tr>
-                                    </table>
-                                </tr>
-                            </div>
-                            </tbody>
-                        </table>
+                    <div v-if="testgroup.testcase">
+                        <md-table md-card>
+                            <md-table-row>
+                                <md-table-head style="width:25%;">TestCase ID</md-table-head>
+                                <md-table-head style="width:25%;">Name</md-table-head>
+                                <md-table-head>Result</md-table-head>
+                            </md-table-row>
+                            <md-table-row v-for="(item,key) in testgroup.testcase" :key="key" >
+                                <md-table-cell style="width:25%;"><span :data-tooltip="item.description._cdata">{{ item._attributes?item._attributes.id:'no-id' }}</span></md-table-cell>
+                                <md-table-cell style="width:50%;">{{ item._attributes?item._attributes.name:'no-name'}}</md-table-cell>
+                                <md-table-cell :style="'text-align:center;width:25%;background-color:'+(Array.isArray(item.testrun)?(item.testrun.filter(elt=>{return elt.result._text.includes('ok')}).length===item.testrun.length?'#00FF00;':'red;'):(item.testrun.result&&item.testrun.result._text.includes('ok')?'#00FF00;':'red;'))">{{ Array.isArray(item.testrun)?(item.testrun.filter(elt=>{return elt.result._text.includes('ok')}).length===item.testrun.length?'OK':'FAIL'):item.testrun.result._text}}</md-table-cell>
+                            </md-table-row>
+                        </md-table>
                     </div>
               </div>
             </div>
         </div>
+        
     </div>
 </template>
 <script>
-/* eslint-disable */
 export default {
     name:'RecursiveAccordion',
     props:{
@@ -99,9 +69,6 @@ export default {
 
             })
         }
-        $(".fold-table tr.view").on("click", function(){
-            $(this).toggleClass("open").next(".fold").toggleClass("open");
-        });
     }
 }
 </script>
@@ -111,66 +78,5 @@ export default {
 }
 .accordion-header {
     margin: 0 !important;
-}
-table {
-	 width: 100%;
-}
- table th {
-	 text-align: left;
-	 border-bottom: 1px solid #ccc;
-}
- table th, table td {
-	 padding: 0.4em;
-}
- table.fold-table > tbody > tr.view td, table.fold-table > tbody > tr.view th {
-	 cursor: pointer;
-}
- table.fold-table > tbody > tr.view td:first-child, table.fold-table > tbody > tr.view th:first-child {
-	 position: relative;
-	 padding-left: 20px;
-}
- table.fold-table > tbody > tr.view td:first-child:before, table.fold-table > tbody > tr.view th:first-child:before {
-	 position: absolute;
-	 top: 50%;
-	 left: 5px;
-	 width: 9px;
-	 height: 16px;
-	 margin-top: -8px;
-	 font: 16px fontawesome;
-	 color: #999;
-	 content: "\f0d7";
-	 transition: all 0.3s ease;
-}
- table.fold-table > tbody > tr.view:nth-child(4n-1) {
-	 background: #eee;
-}
- table.fold-table > tbody > tr.view:hover {
-	 background: #000;
-}
- table.fold-table > tbody > tr.view.open {
-	 background: tomato;
-	 color: white;
-}
- table.fold-table > tbody > tr.view.open td:first-child:before, table.fold-table > tbody > tr.view.open th:first-child:before {
-	 transform: rotate(-180deg);
-	 color: #333;
-}
- table.fold-table > tbody > tr.fold {
-	 display: none;
-}
- table.fold-table > tbody > tr.fold.open {
-	 display: table-row;
-}
- .fold-content {
-	 padding: 0.5em;
-}
- .fold-content h3 {
-	 margin-top: 0;
-}
- .fold-content > table {
-	 border: 2px solid #ccc;
-}
- .fold-content > table > tbody tr:nth-child(even) {
-	 background: #eee;
 }
 </style>
